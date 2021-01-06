1ce6d657a2b0acdd1522448f06f1a8f512f9226e
Jennifer Lam <jlam86@gmail.com>
2021-01-06 12:24:43 +1100

    Add Yodlee module with API calls

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { FroppaCard, SpendGrid } from '../../components';
+import { Yodlee } from '../../utils/Yodlee';
 import './HomePage.css';
+
+// test Yodlee API calls for account and transaction data
+Yodlee.getTokenInfo();
 
 // to convert to functional component if no state or props used
 class HomePage extends React.PureComponent<Record<string, unknown>, Record<string, unknown>> {
